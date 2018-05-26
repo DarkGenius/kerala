17eba8fa22b2820e87598ccb2cacc1e70e9caeaf
DarkGenius <darkgenius@nextmail.ru>
2018-05-26 22:03:27 +0300

    Fix package path

--- ipfs.go
+++ ipfs.go
@@ -4,9 +4,9 @@ import (
 
     "github.com/jbenet/go-ipfs/core"
     "github.com/jbenet/go-ipfs/repo/fsrepo"
-    "code.google.com/p/go.net/context"
+    "golang.org/x/net/context"
 	"fmt"
-	u "github.com/jbenet/go-ipfs/util"
+	u "github.com/ipfs/go-ipfs-util"
 	merkledag "github.com/jbenet/go-ipfs/merkledag"
     "net/http"
 	"bytes"
